b6495f031311c59cfa4f5479c99f86a3b49ca7f2
Stephanie Williams <stephaniewilliams@MacBookPro.wowway.com>
2025-07-24 12:29:07 -0400

    better icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,15 +14,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.json" />
+        {/* Standard favicon */}
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+
+        {/* Additional favicon sizes */}
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+
+        {/* Theme colors and mobile app settings */}
         <meta name="theme-color" content="#4facfe" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Swim Tracker" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
-        <link rel="icon" href="/icon-192.png" />
       </head>
       <body>{children}</body>
     </html>
